e80fe27b2fd57170edfdb7419775633035888373
Felipe Zipitria <felipe.zipitria@owasp.org>
2024-11-03 22:29:00 +0000

    chore: update antlr version
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- parser/generate.sh
+++ parser/generate.sh
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-alias antlr4='java -Xmx500M -cp "../lib/antlr-4.13.0-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias antlr4='java -Xmx500M -cp "../lib/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 antlr4 -Dlanguage=Go -no-visitor -package parsing -o ../parsing *.g4
 antlr4 -Dlanguage=Python3 -no-visitor -package parsing -o ../parsing *.g4
