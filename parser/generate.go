44b3df9968fbadc373e42c514e457fa44ae33d9d
Felipe Zipitria <felipe.zipitria@owasp.org>
2025-03-18 16:18:13 -0300

    chore: remove moved directory
    Signed-off-by: Felipe Zipitria <felipe.zipitria@owasp.org>

--- parser/generate.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2023 Felipe Zipitria
-// SPDX-License-Identifier: Apache-2.0
-
-package parser
-
-//go:generate ./generate.sh
--- parser/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Copyright 2023 Felipe Zipitria
-# SPDX-License-Identifier: Apache-2.0
-
-
-alias antlr4='java -Xmx500M -cp "../lib/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-antlr4 -Dlanguage=Go -no-visitor -package parsing -o ../parsing *.g4
-antlr4 -Dlanguage=Python3 -no-visitor -package parsing -o ../parsing *.g4
